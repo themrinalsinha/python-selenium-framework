69c958a31a3e51e1512a06a5251054aafd5b7550
Mrinal Sinha <mail@themrinalsinha.com>
2018-01-29 23:12:29 +0530

    temp change

--- framework/__init__.py
+++
@@ -1,6 +0,0 @@
-def terminal_output(command):
-    from subprocess import check_output
-    command = command.split(' ')
-    return check_output(command).decode('utf-8').strip()
-
-from .automator import Automator
